80762b80bb5f415789dd1b9e323a6452d8cf4df4
jomorenoo <jomorenoo@unal.edu.co>
2024-03-12 10:37:03 -0500

    intento2

--- pregunta.py
+++ pregunta.py
@@ -5,7 +5,7 @@ Ingestión de datos - Reporte de clusteres
 Construya un dataframe de Pandas a partir del archivo 'clusters_report.txt', teniendo en
 cuenta que los nombres de las columnas deben ser en minusculas, reemplazando los espacios
 por guiones bajos; y que las palabras clave deben estar separadas por coma y con un solo 
-espacio entre palabra y palabra.
+espacio entre palabra y palabra
 
 
 """
