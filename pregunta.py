3e1779957fe2fb80f13c8728047ef6f671da10a9
jomorenoo <jomorenoo@unal.edu.co>
2024-03-12 10:31:12 -0500

    intento1

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,32 @@ espacio entre palabra y palabra.
 
 
 """
+
 import pandas as pd
 
 
 def ingest_data():
+    # Defino el archivo como variable y lo leo
+    archivo = "clusters_report.txt"
 
-    #
-    # Inserte su código aquí
-    #
+    with open(archivo, "r") as f:
+        lineas = f.readlines()
+
+    data = []
+    for line in lineas:
+
+        words = line.strip().split()
+        keywords = ", ".join(words[3:])
+
+        row = {
+            "cluster_id": words[0],
+            "cluster_size": words[1],
+            "centroid": words[2],
+            "keywords": keywords,
+        }
+        data.append(row)
+
+    df = pd.Dataframe(data)
+    df.columns = df.columns.str.lower().str.replace(" ", "_")
 
     return df
